2ddd03cf0be572fb2976d36ac0e8348a65b64095
losmescaleros <losmescaleros@gmail.com>
2015-01-15 12:35:44 -0500

    Added main header and example inputs and outputs

---
+++ assn_1/main.h
@@ -0,0 +1,6 @@
+#define IN_MEMORY_LINEAR_SEARCH "--mem-lin"
+#define IN_MEMORY_BINARY_SEARCH "--mem-bin"
+#define ON_DISK_LINEAR_SEARCH "--disk-lin"
+#define ON_DISK_BINARY_SEARCH "--disk-bin"
+
+#define ERROR -1
